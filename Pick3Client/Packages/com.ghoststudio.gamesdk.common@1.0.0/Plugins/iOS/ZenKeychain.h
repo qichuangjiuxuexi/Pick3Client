cf1350b4d92fb6cf00692a14eeeab7c8cf5cc291
qiuguoliang <guoliang.qiu@me2zen.com>
2024-01-11 19:05:00 +0800

    引入ghoststudio sdk 处理报错

--- Pick3Client/Packages/com.ghoststudio.gamesdk.common@1.0.0/Plugins/iOS/ZenKeychain.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface ZenKeychain : NSObject
-+ (void)save:(NSString *)service data:(id)data;
-+ (id)load:(NSString *)service;
-+ (void)remove:(NSString *)service;
-@end
--- Pick3Client/Packages/com.ghoststudio.gamesdk.ui@1.0.0/Scripts/Scene/UIScene.cs
+++
@@ -1,9 +0,0 @@
-namespace AppBase.UI.Scene
-{
-    /// <summary>
-    /// UI场景，会挂在Canvas下
-    /// </summary>
-    public class UIScene : SceneBase
-    {
-    }
-}
